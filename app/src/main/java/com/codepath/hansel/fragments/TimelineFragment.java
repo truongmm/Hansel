eff8412051d26e9092491b611e0c33f0b254f097
truongmm <truongmm@gmail.com>
2015-11-25 20:53:38 -0800

    Added Hamburger Menu

---
+++ app/src/main/java/com/codepath/hansel/fragments/TimelineFragment.java
@@ -0,0 +1,69 @@
+package com.codepath.hansel.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.astuetz.PagerSlidingTabStrip;
+import com.codepath.hansel.R;
+import com.codepath.hansel.models.Pebble;
+import com.codepath.hansel.models.User;
+import com.codepath.hansel.utils.DatabaseHelper;
+
+import java.util.ArrayList;
+
+public class TimelineFragment extends Fragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.activity_log, container, false);
+        setupViews(view);
+        return view;
+    }
+
+    private void setupViews(View view) {
+        ViewPager viewPager = (ViewPager) view.findViewById(R.id.viewPager);
+        viewPager.setAdapter(new LogPagerAdapter(getActivity().getSupportFragmentManager()));
+
+        PagerSlidingTabStrip tabStrip = (PagerSlidingTabStrip) view.findViewById(R.id.tabs);
+        tabStrip.setViewPager(viewPager);
+    }
+
+    public class LogPagerAdapter extends FragmentPagerAdapter {
+
+        private String[] tabTitles = {"Friends", "You"};
+
+        public LogPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            if (position == 0)
+                return new FriendsLogFragment();
+            else if (position == 1)
+                return new YouLogFragment();
+            else
+                return null;
+        }
+
+        public CharSequence getPageTitle(int position) {
+            return tabTitles[position];
+        }
+
+        @Override
+        public int getCount() {
+            return tabTitles.length;
+        }
+    }
+}
